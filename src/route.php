9addc328ce63a9f6e51568d732f874e7b8c3b216
Toni Listiyo <l.toni2007@gmail.com>
2025-01-27 14:36:21 +0700

    update check array routes

--- src/route.php
+++ src/route.php
@@ -3,7 +3,8 @@
 function activeUrl($routeName){
 
     if(is_array($routeName)){
-        return in_array(request()->route()->getName(), $routeName);
+        // return in_array(request()->route()->getName(), $routeName);
+        return collect($routeName)->filter(fn($name) => request()->routeIs($name))->isNotEmpty();
     }
 
     return request()->routeIs($routeName);
